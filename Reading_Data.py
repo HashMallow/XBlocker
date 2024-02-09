0c73ab84411255bc954b044334dc7f32c88d38ae
hashmallow <26327935+HashMallow@users.noreply.github.com>
2024-02-08 18:34:21 -0800

    Adding the Cleaner.ipynb File

--- Reading_Data.py
+++ Reading_Data.py
@@ -5,6 +5,6 @@ def get_data():
     df = pd.read_csv(r"C:\Users\malir\OneDrive\Old laptop\Find\scores.csv")
     df.sort_values(ascending=False, by="score", inplace=True)
 
-    index = 460
-    df = df[df["score"] > 2][index:1000]
+    index = 520
+    df = df[df["score"] > 2][index:]
     return df["screen_name"].tolist(), index
